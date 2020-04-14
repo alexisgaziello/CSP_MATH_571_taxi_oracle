1cd442a11a530dc3ee4ac702efb7bcc291b0747f
Alexis Gaziello <alexis.gaziello@gmail.com>
2020-04-14 18:52:17 -0500

    add model example

---
+++ src/models/model_name/model_name.py
@@ -0,0 +1,24 @@
+import pickle # NO CHANGE
+
+# To save model:
+# Calculate it and then save it with:
+pickle.dump(model, open('model_name.pkl','wb'))
+
+
+import # necessary libraries to load the model for example import skleanr.whatever.
+
+def model_name_Load(): # NO CHANGE
+    return pickle.load(open('model.pkl','rb')) # NO CHANGE
+
+def model_name_TransformDataToX(data): # NO CHANGE
+    #Perform calculations. Data is a row of the selected date.
+    return X # NO CHANGE
+
+def model_name_TransformYToResult(Y): # NO CHANGE
+    # Perform neccesary transformations. Result  is an array of size 77 in which each item
+    #  it's the value of taxi trips for each community in order 
+    # (meaning first value is for community 1, second for community 2, etc) 
+    # OR a dictionary with the key as the community
+    #  and the value the predicted trips.
+
+    return result # NO CHANGE
