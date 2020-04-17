85f8eb8a46ba8b94bb04f6a3d8c1e693515f3419
Julen Arizaga <jarizagaechebarria@hawk.iit.edu>
2020-04-16 20:30:20 -0500

    Modified for new dataset

---
+++ src/models/neuralNet/model_name.py
@@ -0,0 +1,26 @@
+import joblib # NO CHANGE
+
+# To save model:
+# Calculate it and then save it with:
+# you don't have to call the following gunction here. call it in your script
+joblib.dump(model, open('model_name.pkl','wb'))
+
+
+import 
+
+
+
+# necessary libraries to load the model for example import skleanr.whatever.
+
+def TransformDataToX(df, date): # NO CHANGE
+    #Perform calculations. Data is a row of the selected date.
+    return X # NO CHANGE
+
+def TransformYToResult(Y): # NO CHANGE
+    # Perform neccesary transformations. Result  is an array of size 77 in which each item
+    #  it's the value of taxi trips for each community in order 
+    # (meaning first value is for community 1, second for community 2, etc) 
+    # OR a dictionary with the key as the community
+    #  and the value the predicted trips.
+
+    return result # NO CHANGE
