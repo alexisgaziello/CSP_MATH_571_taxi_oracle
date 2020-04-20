49e97d9470a6426544a9ec952d9986355859edad
Alexis Gaziello <alexis.gaziello@gmail.com>
2020-04-19 21:27:00 -0500

    update

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -16,7 +16,7 @@ else:
 
 # Paths
 path = os.path.dirname(sys.argv[0])
-pathToSrc =  path[:path.find('src')+3]
+pathToSrc =  path[:path.find('src')+4]
 pathToGDrive =  path[:path.find('Code')]
 pathToDatasets = pathToGDrive + f'DataSets{sep}'
 
@@ -43,9 +43,9 @@ defaultDate = df.iloc[int(len(df)/2),].trip_start_timestamp[:10]
 # Random Forest
 print("\nLoading Random Forest...")
 
-model_randomForest = joblib.load(pathToDatasets+"/model/randomForest.pkl")
+model_randomForest = joblib.load(pathToDatasets+f"{sep}model{sep}randomForest.pkl")
 
-path_to_model = pathToSrc + f'models{sep}/randomForest/randomForest.py'
+path_to_model = pathToSrc + f'models{sep}randomForest{sep}randomForest.py'
 spec = importlib.util.spec_from_file_location("randomForest", path_to_model)
 randomForest = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(randomForest)
@@ -53,7 +53,7 @@ spec.loader.exec_module(randomForest)
 print("\nLoading Regression...")
 model_regression = joblib.load(pathToDatasets + "/model/regression.pickle")
 
-path_to_model = pathToSrc + f'models{sep}/regression/regression.py'
+path_to_model = pathToSrc + f'models{sep}regression/regression.py'
 spec = importlib.util.spec_from_file_location("regression", path_to_model)
 regression = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(regression)
@@ -62,7 +62,7 @@ spec.loader.exec_module(regression)
 print("\nLoading Gradient BR...")
 model_gradientBoost = joblib.load(pathToDatasets+"/model/GBR.pickle")
 
-spec = importlib.util.spec_from_file_location("gradientBoost", pathToSrc + f'models{sep}/gradientBoost/gradientBoost.py')
+spec = importlib.util.spec_from_file_location("gradientBoost", pathToSrc + f'models{sep}gradientBoost/gradientBoost.py')
 gradientBoost = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(gradientBoost)
 
@@ -118,33 +118,36 @@ def predict():
     #result = mp.mapGenerator(prediction, communities=communities, saveByte=True)    
 
     # Real Y (values)
-    real = df.loc[df.trip_start_timestamp == "2019-11-01 00:00:00"]['Trips']
+    values = df.loc[df.trip_start_timestamp == date]['Trips'].values
+    real = {}
+    for i in range(len(values)):
+        real[i] = values[i]
     real_img = mp.mapGenerator(real, communities=communities, saveByte=True) 
+
+    # Regression
+    print("Regression")
+    X = randomForest.TransformDataToX(df.copy(), date)
+    Y = model_regression.predict(X)
+    prediction = regression.TransformYToResult(Y)
+    regression_img = mp.mapGenerator(prediction, communities=communities, saveByte=True)
 
     # Random Forest
     print("Random Forest")
-    X = randomForest.TransformDataToX(df, date)
+    X = randomForest.TransformDataToX(df.copy(), date)
     Y = model_randomForest.predict(X)
     prediction = randomForest.TransformYToResult(Y)
     randomForest_img = mp.mapGenerator(prediction, communities=communities, saveByte=True)
     
-    # Regression
-    print("Regression")
-    X = randomForest.TransformDataToX(df, date)
-    Y = model_regression.predict(X)
-    prediction = regression.TransformYToResult(Y)
-    regression_img = mp.mapGenerator(prediction, communities=communities, saveByte=True)
-    
     # Gradient Boosting
     print("Gradient Boosting")
-    X = randomForest.TransformDataToX(df, date)
+    X = randomForest.TransformDataToX(df.copy(), date)
     Y = model_gradientBoost.predict(X)
     prediction = gradientBoost.TransformYToResult(Y)
     gradientBoost_img = mp.mapGenerator(prediction, communities=communities, saveByte=True)
     
     # Neural Net
     print("Neural Net")
-    X = neuralNet.TransformDataToX(df, date)
+    X = neuralNet.TransformDataToX(df.copy(), date)
     Y = model_neural_network.predict(X)
     prediction = neuralNet.TransformYToResult(Y)
     neuralNet_img = mp.mapGenerator(prediction, communities=communities, saveByte=True)
@@ -180,4 +183,4 @@ def results():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
